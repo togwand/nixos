ed3a7dce49c13e1f21a05df6d8a0fd0b9181593e
togwand <togwand@gmail.com>
2024-10-31 02:16:29 -0300

    seeing if legacy is better than none, retesting blue theme

--- stale/nixos.nix
+++ stale/nixos.nix
@@ -38,7 +38,7 @@
     };
     plymouth = {
       enable = true;
-      theme = "fade-in";
+      theme = "script";
     };
     initrd = {
       kernelModules = [
@@ -154,7 +154,7 @@
       allowBitmaps = false;
       subpixel = {
         rgba = "none";
-        lcdfilter = "none";
+        lcdfilter = "legacy";
       };
       hinting = {
         style = "medium";
