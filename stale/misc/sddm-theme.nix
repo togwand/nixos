962def583bebfe4ffd00f34e660b81040dc98f5e
togwand <togwand@gmail.com>
2024-10-31 17:55:54 -0300

    Updated file to now use hash instead of incorrect sha256

--- stale/misc/sddm-theme.nix
+++ stale/misc/sddm-theme.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation {
     owner = "Keyitdev";
     repo = "sddm-astronaut-theme";
     rev = "8993670e73d36f4e8edc70d13614fa05edc2575c";
-    sha256 = "sha256-uEZX5J9uhPKakVFnjfzxjOvrvk4F4CNXthkIYroITl4=";
+    hash = "sha256-uEZX5J9uhPKakVFnjfzxjOvrvk4F4CNXthkIYroITl4=";
   };
   installPhase = ''
     mkdir -p $out
