f306fc39e3b9aedb164f1eb054f38b2b1142c5f3
togwand <togwand@gmail.com>
2024-11-01 12:32:24 -0300

    Testing plymouth without autologin

--- stale/nixos.nix
+++ stale/nixos.nix
@@ -42,7 +42,7 @@
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = true;
     };
-    plymouth.enable = false;
+    plymouth.enable = true;
     tmp.cleanOnBoot = true;
     initrd = {
       verbose = false;
