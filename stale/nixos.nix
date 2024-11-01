2abde3c421f960b74a486446724cbaa034d46167
togwand <togwand@gmail.com>
2024-10-31 23:24:06 -0300

    Plymouth has weird glithches half of the time that slow down the startup for some reason, so I will try with it disabled, even if I see logs messages (maybe try to reduce them even more or enable them all if they don't affect speed)

--- stale/nixos.nix
+++ stale/nixos.nix
@@ -37,7 +37,7 @@
       systemd-boot.enable = true;
     };
     plymouth = {
-      enable = true;
+      enable = false;
       theme = "abstract_ring";
       themePackages = with pkgs; [
         (adi1090x-plymouth-themes.override {selected_themes = ["abstract_ring"];})
