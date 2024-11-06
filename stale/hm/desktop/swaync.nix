c5e4c47932900aac7844f6f3afdb609798ff3b24
togwand <togwand@gmail.com>
2024-11-06 16:24:48 -0300

    Added timeouts to the notifications of swaync because they stay for way too much time, trying out the wiki on my local hw config, also trying without ntfs3g package added since the supported filesystem line should make it available

--- stale/hm/desktop/swaync.nix
+++ stale/hm/desktop/swaync.nix
@@ -4,6 +4,10 @@
     settings = {
       positionX = "center";
       positionY = "top";
+      layer = "overlay";
+      timeout = "3";
+      timeout-low = "1";
+      timeout-critical = "7";
     };
   };
 }
--- stale/nixos.nix
+++ stale/nixos.nix
@@ -132,7 +132,7 @@
   environment = {
     systemPackages = with pkgs; [
       exfat
-      ntfs3g
+      # ntfs3g
     ];
     variables = {
       VISUAL = "nvim";
