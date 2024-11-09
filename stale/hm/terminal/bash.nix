70a35e17c476dcdb330c41311c300b65bd267177
togwand <togwand@gmail.com>
2024-11-09 09:38:33 -0300

    Added very basic zsh config to home manager and simplified bash one (also fixed the history in bash to be more to my liking) because if I ever use bash I would like it to be as close to default as possible because that's what it's good for I guess, I'm already loving barebones for the vi mode and the highlighting and history autocomplete stuff

--- stale/hm/terminal/bash.nix
+++ stale/hm/terminal/bash.nix
@@ -1,39 +1,16 @@
 {...}: {
   programs.bash = {
     enable = true;
-    enableCompletion = false;
-    historyControl = ["erasedups"];
-    shellAliases = {
-      ".." = "cd ..";
-      tflk = "sudo nixos-rebuild test --impure --flake .";
-      bflk = "sudo nixos-rebuild boot --impure --flake .";
-      sflk = "sudo nixos-rebuild switch --impure --flake .";
-      ra = "ranger";
-    };
+    enableCompletion = true;
+    historyControl = ["ignoredups"];
+    shellAliases = {};
     shellOptions = [
-      "assoc_expand_once"
-      "autocd"
-      "cdspell"
       "checkjobs"
       "checkwinsize"
-      "cmdhist"
-      "complete_fullquote"
       "dotglob"
-      "expand_aliases"
       "extglob"
       "globstar"
       "histappend"
-      "histverify"
-      "interactive_comments"
-      "lithist"
-      "nocaseglob"
-      "nocasematch"
-      "nullglob"
-      "promptvars"
-      "shift_verbose"
-      "sourcepath"
-      "varredir_close"
-      "xpg_echo"
     ];
   };
 }
--- stale/hm/terminal/zsh.nix
+++ stale/hm/terminal/zsh.nix
@@ -1,2 +1,23 @@
 {...}: {
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    autosuggestion = {
+      enable = true;
+      strategy = [
+        "history"
+        "completion"
+      ];
+    };
+    defaultKeymap = "vicmd";
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ".." = "cd ..";
+      tflk = "sudo nixos-rebuild test --impure --flake .";
+      bflk = "sudo nixos-rebuild boot --impure --flake .";
+      sflk = "sudo nixos-rebuild switch --impure --flake .";
+      ra = "ranger";
+    };
+  };
 }
