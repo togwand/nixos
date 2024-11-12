4e61eceda80286aaa908d0d7573b8b0d0d67d56b
togwand <togwand@gmail.com>
2024-11-12 00:53:42 -0300

    Trying to fix an issue with teh new alias to create and burn at the same time, it might be the rm -r command but not sure yet

--- stale/hm/terminal/zsh.nix
+++ stale/hm/terminal/zsh.nix
@@ -78,7 +78,7 @@
       "reload-hwcfg" = "sudo nixos-generate-config && patch-hwcfg";
       "d-flake-format" = "nix fmt";
       "d-flake-update" = "nix flake update";
-      "d-generate-iso-result" = "nix run nixpkgs#nixos-generators -- --format iso --flake github:togwand/nixos-install#fast -o result";
+      "d-generate-iso-result" = "nix run nixpkgs#nixos-generators -- --format iso --flake github:togwand/nixos-install#fast -o result && echo 'ISO generated, you can now copy it into a removable storage device'";
       "d-burn-result-iso" = "sudo cp result/iso/nixos-*.iso \ ";
       "d-create-and-burn-iso" = "d-generate-iso-result && d-burn-result-iso && sudo rm -r ./result";
     };
