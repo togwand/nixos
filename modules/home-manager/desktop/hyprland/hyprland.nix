4941ec2e5ba2093698c5e0eec610ef0ead56f57e
togwand <togwand@gmail.com>
2024-12-01 15:00:35 -0300

    Changed back to 100hz because it's the sweet spot, also hyprland shadow changed to a single corner border and some options changed as well, disabled hyprcursor because I can't declare it with nix it seems, need to check how to theme the system better after fixing the shell manager.

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -10,180 +10,73 @@
       enable = true;
       xwayland.enable = true;
       settings = {
-        "$app-launcher" = "tofi-drun";
+        # Variables
         "$terminal-emulator" = "foot";
         "$web-browser" = "firefox";
-
-        general = {
-          border_size = 0;
-          no_border_on_floating = true;
-          gaps_in = "8,8,8,8";
-          gaps_out = "16,16,16,16";
-          gaps_workspaces = 0;
-          layout = "dwindle";
-          no_focus_fallback = true;
-          resize_on_border = true;
-          extend_border_grab_area = 32;
-          hover_icon_on_border = true;
-          allow_tearing = true;
-          resize_corner = 0;
-          snap = {
-            enabled = false;
-          };
-        };
-
-        decoration = {
-          rounding = 0;
-          active_opacity = 1.0;
-          inactive_opacity = 1.0;
-          fullscreen_opacity = 1.0;
-          dim_inactive = false;
-          dim_strength = 0.0;
-          blur = {
-            enabled = true;
-            size = 3;
-            passes = 1;
-            ignore_opacity = false;
-            new_optimizations = true;
-            xray = true;
-            noise = 0.0;
-            contrast = 2.0;
-            brightness = 1.0;
-            vibrancy = 0.5;
-            vibrancy_darkness = 0.15;
-            special = false;
-            popups = false;
-          };
-          shadow = {
-            enabled = true;
-            range = 24;
-            render_power = 3;
-            sharp = false;
-            ignore_window = true;
-            color = "rgba(2D48B9dd)";
-            color_inactive = "rgba(333333aa)";
-            scale = 1.0;
-          };
-        };
-
-        animations = {
-          enabled = true;
-          first_launch_animation = true;
-        };
-
-        bezier = [ "custom, 0, 0.7, 0.7, 1" ];
-        animation = [
-          "global, 1, 2, custom"
-          "workspaces, 1, 2, custom, slidefadevert 15%"
-          "windows, 1, 2, custom, popin 55%"
-          "layers, 1, 1, custom, fade"
-          "fadeIn, 0"
-          "fadeShadow, 1, 7, custom"
-        ];
-
-        input = {
-          kb_layout = "latam";
-          kb_options = "caps:swapescape";
-          numlock_by_default = false;
-          resolve_binds_by_sym = false;
-          repeat_rate = 60;
-          repeat_delay = 160;
-          force_no_accel = true;
-          natural_scroll = false;
-          follow_mouse = "0";
-          focus_on_close = "0";
-          mouse_refocus = false;
-          float_switch_override_focus = "0";
-          special_fallthrough = false;
-          off_window_axis_events = "3";
-          emulate_discrete_scroll = "0";
-        };
-
-        group = {
-          auto_group = false;
-          groupbar = {
-            enabled = false;
-          };
-        };
-
-        misc = {
-          disable_hyprland_logo = true;
-          disable_splash_rendering = true;
-          font_family = "monospace";
-          vfr = true;
-          vrr = "0"; # Adaptive Sync
-          always_follow_on_dnd = false;
-          layers_hog_keyboard_focus = true;
-          animate_manual_resizes = false;
-          animate_mouse_windowdragging = false;
-          disable_autoreload = true;
-          enable_swallow = false;
-          focus_on_activate = true;
-          new_window_takes_over_fullscreen = "1";
-          exit_window_retains_fullscreen = true;
-          initial_workspace_tracking = "0";
-          middle_click_paste = false;
-          render_unfocused_fps = 1;
-        };
-
-        binds = {
-          pass_mouse_when_bound = true;
-          scroll_event_delay = 160;
-          workspace_back_and_forth = false;
-          allow_workspace_cycles = false;
-          workspace_center_on = "1";
-          focus_preferred_method = "0";
-          movefocus_cycles_fullscreen = false;
-          disable_keybind_grabbing = false;
-          window_direction_monitor_fallback = true;
-        };
-
-        xwayland = {
-          enabled = true;
-          use_nearest_neighbor = false;
-          force_zero_scaling = false;
-        };
-
-        opengl = {
-          nvidia_anti_flicker = false;
-          force_introspection = "1";
-        };
-
-        render = {
-          explicit_sync = "1";
-          explicit_sync_kms = "1";
-          direct_scanout = false;
-          expand_undersized_textures = true;
-        };
-
-        cursor = {
-          sync_gsettings_theme = true;
-          no_hardware_cursors = "1";
-          no_break_fs_vrr = true;
-          min_refresh_rate = 60;
-          hotspot_padding = 0;
-          inactive_timeout = 0;
-          no_warps = true;
-          persistent_warps = false;
-          warp_on_change_workspace = false;
-          enable_hyprcursor = true;
-          hide_on_key_press = false;
-        };
-
-        # Keywords
-
-        exec-once = "waybar";
-        env = [
-          "XCURSOR_SIZE,24"
-          "HYPRCURSOR_SIZE,12"
-        ];
-
-        monitor = ",1920x1080@60.00,0x0,1";
+        "$app-launcher" = "tofi-drun";
+        "$status-bar" = "waybar";
         "$workspace" = "SUPER";
         "$window" = "ALT";
         "$window-focus" = "$window+CTRL";
         "$window-resize" = "$window+SHIFT";
         "$window-move" = "$window+CTRL+SHIFT";
+
+        # Keywords
+        monitor = ",1920x1080@99.93,0x0,1";
+        env = [
+          "XDG_SESSION_TYPE,wayland"
+          "XDG_CURRENT_DESKTOP,Hyprland"
+          "XDG_SESSION_DESKTOP,Hyprland"
+        ];
+        exec-once = [
+          "$status-bar"
+          "[workspace 1 silent] $web-browser"
+          "[workspace 2 silent] $terminal-emulator"
+        ];
+
+        bezier = [ "custom, 0, 0.7, 0.7, 1" ];
+        animation = [
+          "global, 1, 2, custom"
+          "windows, 1, 2, custom, popin 55%"
+          "layers, 1, 1, custom, fade"
+          "fadeIn, 0"
+          "border, 1, 7, custom"
+          "borderangle, 0"
+          "workspaces, 1, 2, custom, slidefadevert 15%"
+        ];
+
+        bindm = [
+          "$window, mouse:272, movewindow"
+        ];
+
+        bindl = [
+          ", Print, exec, hyprshot -m region -t 1000"
+          "$workspace , c, exec, hyprpicker -a -f hex"
+          "$workspace , p, exec, hyprshot -m output -m active -t 2000"
+          "$window , p, exec, hyprshot -m window -m active -t 2000"
+        ];
+
+        binde = [
+          "$workspace, k, workspace, r-1"
+          "$workspace, j, workspace, r+1"
+
+          "$window, Tab, cyclenext"
+
+          "$window-resize, h, resizeactive, -24 0"
+          "$window-resize, j, resizeactive, 0  24"
+          "$window-resize, k, resizeactive, 0 -24"
+          "$window-resize, l, resizeactive, 24 0"
+
+          "$window-focus, h, movefocus, l"
+          "$window-focus, j, movefocus, d"
+          "$window-focus, k, movefocus, u"
+          "$window-focus, l, movefocus, r"
+
+          "$window-move, h, moveactive, -24 0"
+          "$window-move, j, moveactive, 0 24"
+          "$window-move, k, moveactive, 0 -24"
+          "$window-move, l, moveactive, 24 0"
+        ];
 
         bind = [
           "$workspace, Delete, exit"
@@ -227,39 +120,6 @@
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
         ];
 
-        binde = [
-          "$workspace, k, workspace, r-1"
-          "$workspace, j, workspace, r+1"
-
-          "$window, Tab, cyclenext"
-
-          "$window-resize, h, resizeactive, -24 0"
-          "$window-resize, j, resizeactive, 0  24"
-          "$window-resize, k, resizeactive, 0 -24"
-          "$window-resize, l, resizeactive, 24 0"
-
-          "$window-focus, h, movefocus, l"
-          "$window-focus, j, movefocus, d"
-          "$window-focus, k, movefocus, u"
-          "$window-focus, l, movefocus, r"
-
-          "$window-move, h, moveactive, -24 0"
-          "$window-move, j, moveactive, 0 24"
-          "$window-move, k, moveactive, 0 -24"
-          "$window-move, l, moveactive, 24 0"
-        ];
-
-        bindl = [
-          ", Print, exec, hyprshot -m region -t 1000"
-          "$workspace , c, exec, hyprpicker -a -f hex"
-          "$workspace , p, exec, hyprshot -m output -m active -t 2000"
-          "$window , p, exec, hyprshot -m window -m active -t 2000"
-        ];
-
-        bindm = [
-          "$window, mouse:272, movewindow"
-        ];
-
         dwindle = {
           force_split = "2";
           preserve_split = true;
@@ -267,6 +127,147 @@
           smart_resizing = false;
           permanent_direction_override = true;
           use_active_for_splits = true;
+        };
+
+        general = {
+          border_size = 1;
+          no_border_on_floating = false;
+          gaps_in = "8,8,8,8";
+          gaps_out = "16,16,16,16";
+          gaps_workspaces = 0;
+          "col.inactive_border" = "rgba(333333aa)";
+          "col.active_border" = "rgba(333333aa) rgba(333333aa) rgba(333333aa) rgba(333333aa) rgba(333333aa) rgba(2D48B9cc) 20deg";
+          layout = "dwindle";
+          no_focus_fallback = true;
+          resize_on_border = true;
+          extend_border_grab_area = 32;
+          hover_icon_on_border = true;
+          allow_tearing = true;
+          resize_corner = 0;
+          snap = {
+            enabled = false;
+          };
+        };
+
+        decoration = {
+          rounding = 12;
+          active_opacity = 1.0;
+          inactive_opacity = 1.0;
+          fullscreen_opacity = 1.0;
+          dim_inactive = false;
+          dim_strength = 0.0;
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            ignore_opacity = false;
+            new_optimizations = true;
+            xray = true;
+            noise = 0.0;
+            contrast = 2.0;
+            brightness = 1.0;
+            vibrancy = 0.5;
+            vibrancy_darkness = 0.15;
+            special = false;
+            popups = false;
+          };
+          shadow = {
+            enabled = false;
+          };
+        };
+
+        animations = {
+          enabled = true;
+          first_launch_animation = true;
+        };
+
+        input = {
+          kb_layout = "latam";
+          kb_options = "caps:swapescape";
+          numlock_by_default = false;
+          resolve_binds_by_sym = false;
+          repeat_rate = 100;
+          repeat_delay = 160;
+          force_no_accel = true;
+          natural_scroll = false;
+          follow_mouse = "0";
+          focus_on_close = "0";
+          mouse_refocus = false;
+          float_switch_override_focus = "0";
+          special_fallthrough = false;
+          off_window_axis_events = "3";
+          emulate_discrete_scroll = "0";
+        };
+
+        group = {
+          auto_group = false;
+          groupbar = {
+            enabled = false;
+          };
+        };
+
+        misc = {
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+          font_family = "monospace";
+          vfr = true;
+          vrr = "0"; # Adaptive Sync
+          always_follow_on_dnd = true;
+          layers_hog_keyboard_focus = true;
+          animate_manual_resizes = false;
+          animate_mouse_windowdragging = false;
+          disable_autoreload = true;
+          enable_swallow = false;
+          focus_on_activate = true;
+          new_window_takes_over_fullscreen = "1";
+          exit_window_retains_fullscreen = true;
+          initial_workspace_tracking = "0";
+          middle_click_paste = false;
+          render_unfocused_fps = 1;
+        };
+
+        binds = {
+          pass_mouse_when_bound = false;
+          scroll_event_delay = 160;
+          workspace_back_and_forth = false;
+          allow_workspace_cycles = false;
+          workspace_center_on = "1";
+          focus_preferred_method = "0";
+          movefocus_cycles_fullscreen = false;
+          disable_keybind_grabbing = false;
+          window_direction_monitor_fallback = false;
+        };
+
+        xwayland = {
+          enabled = true;
+          use_nearest_neighbor = false;
+          force_zero_scaling = false;
+        };
+
+        opengl = {
+          nvidia_anti_flicker = false;
+          force_introspection = "1";
+        };
+
+        render = {
+          explicit_sync = "1";
+          explicit_sync_kms = "1";
+          direct_scanout = false;
+          expand_undersized_textures = true;
+        };
+
+        cursor = {
+          sync_gsettings_theme = true;
+          no_hardware_cursors = "1";
+          no_break_fs_vrr = true;
+          min_refresh_rate = 60;
+          hotspot_padding = 0;
+          inactive_timeout = 0;
+          no_warps = true;
+          persistent_warps = false;
+          warp_on_change_workspace = false;
+          enable_hyprcursor = false;
+          hide_on_key_press = false;
         };
       };
     };
