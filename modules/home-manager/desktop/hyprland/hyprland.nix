1b286ce84fc02599c74944d9d1469a49082bce67
togwand <togwand@gmail.com>
2024-12-04 00:10:11 -0300

    60 is too slow for repeating so trying 120 since it's multiple of 60

--- modules/home-manager/desktop/hyprland/hyprland.nix
+++ modules/home-manager/desktop/hyprland/hyprland.nix
@@ -211,7 +211,7 @@
           kb_options = "caps:swapescape";
           numlock_by_default = false;
           resolve_binds_by_sym = false;
-          repeat_rate = 60;
+          repeat_rate = 120;
           repeat_delay = 160;
           force_no_accel = true;
           natural_scroll = false;
