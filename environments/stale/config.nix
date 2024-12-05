8836a34caa216aa281d4ed5e34448ea8c2469271
togwand <togwand@gmail.com>
2024-12-05 18:07:27 -0300

    Re-added the kernel paremeter because I forgot to add edid/ to each entry, now seeing if putting things there works, or even one...

--- environments/stale/config.nix
+++ environments/stale/config.nix
@@ -27,12 +27,12 @@
           "FHD_144" = "    333.216  1920 1928 1960 2000   1080 1143 1151 1157   +hsync -vsync";
         };
       };
-      outputs = {
-        "DP-1" = {
-          edid = "FHD_144.bin";
-          # mode = "";
-        };
-      };
+      # outputs = {
+      #   "DP-1" = {
+      #     edid = "FHD_144.bin";
+      #     # mode = "";
+      #   };
+      # };
     };
     nvidia = {
       modesetting.enable = true;
@@ -52,6 +52,7 @@
     kernelParams = [
       "quiet"
       "udev.log_level=3"
+      "drm.edid_firmware=DP-1:edid/FHD_144.bin,DP-1:edid/FHD_120.bin,DP-1:edid/FHD_105.bin,DP-1:edid/FHD_90.bin,DP-1:edid/FHD_75.bin"
     ];
     initrd = {
       verbose = false;
